d0c38e9b3387f57606cdd0a9b75716a53eebc0b7
syedsualehabbas <sualehabbas.6981@gmail.com>
2026-01-20 13:19:05 +0500

    adding comments

--- Lab02/Core/Src/main.c
+++ Lab02/Core/Src/main.c
@@ -153,6 +153,21 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+
+  // // TASK 0
+  //  while (1)
+  //   {
+  //       /* USER CODE END WHILE */
+        
+  //       uint8_t msg[] = "Hello, World!\r\n";
+  //       HAL_UART_Transmit(&huart2,
+  //                         msg,
+  //                         sizeof(msg),
+  //                         HAL_MAX_DELAY);
+
+  //       /* USER CODE BEGIN 3 */
+  //   }
+
 while (1)
     {
 
